da675076e3faac3d01c47be316ced42d7368e87c
atalasluis <atalasturey@gmail.com>
2025-12-07 02:44:08 -0400

    corrección de conexión

--- database.py
+++ database.py
@@ -1,10 +1,6 @@
 from pymongo import MongoClient
 
-
-MONGO_URI ="mongodb+srv://mauriciousseglio:mi_clave_123@clusterlast.4ip2mv0.mongodb.net/?appName=ClusterLast"
-
 MONGO_URI = 'mongodb://localhost:27017/'
-
 
 def dbConnection():
     try:
@@ -13,5 +9,4 @@ def dbConnection():
         return db
     except Exception as e:
         print("Error de conexión con la bdd:", e)
-        return None
-    
+        return None   
